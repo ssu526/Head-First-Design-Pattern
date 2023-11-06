df0bd76308c75255033077407e756331622c3654
Sue <sue.su.0526@gmail.com>
2023-11-06 10:43:19 -0500

    Chapter 2

---
+++ Chapter_2_Observer_Pattern/Observer/CurrentConditionsDisplay.java
@@ -0,0 +1,28 @@
+package Chapter_2_Observer_Pattern.Observer;
+
+import Chapter_2_Observer_Pattern.Subject.Subject;
+
+public class CurrentConditionsDisplay implements DisplayElement, Observer {
+    private float temperature;
+    private float humidity;
+    private float pressure;
+    private Subject weatherData;
+
+    public CurrentConditionsDisplay(Subject weatherData){
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Current conditions: " + temperature + "F  " + humidity + "%  " + pressure + "kPa");
+    }
+
+    @Override
+    public void update(float temp, float humidity, float pressure) {
+        this.temperature = temp;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        display();
+    }
+}
---
+++ Chapter_2_Observer_Pattern/Observer/DisplayElement.java
@@ -0,0 +1,5 @@
+package Chapter_2_Observer_Pattern.Observer;
+
+public interface DisplayElement {
+    public void display();
+}
