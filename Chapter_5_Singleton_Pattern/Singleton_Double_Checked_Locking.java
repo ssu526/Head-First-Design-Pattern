d1032c26781c7e21ee3613a622d968785397ebf7
Sue <sue.su.0526@gmail.com>
2023-11-10 12:58:44 -0500

    Chapter 5

---
+++ Chapter_5_Singleton_Pattern/Singleton_Double_Checked_Locking.java
@@ -0,0 +1,18 @@
+package Chapter_5_Singleton_Pattern;
+
+public class Singleton_Double_Checked_Locking {
+    private volatile static Singleton_Double_Checked_Locking uniqueInstance;
+
+    private Singleton_Double_Checked_Locking(){};
+
+    public static Singleton_Double_Checked_Locking getInstance(){
+        if(uniqueInstance == null){
+            synchronized (Singleton.class){
+                if(uniqueInstance == null){
+                    uniqueInstance = new Singleton_Double_Checked_Locking();
+                }
+            }
+        }
+        return uniqueInstance;
+    }
+}
